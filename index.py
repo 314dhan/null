d2dd947d6d7a982cd5f16c2cdfc9e996d3351d41
dhan <ramadhan25321@gmail.com>
2023-09-23 07:33:26 +0700

    del

--- index.py
+++ index.py
@@ -1,17 +1,1 @@
-import random
 
-def tebak_angka():
-    angka_rahasia = random.randint(1, 100)
-    tebakan = None
-
-    while tebakan != angka_rahasia:
-        tebakan = int(input("Tebak angka (1-100): "))
-
-        if tebakan < angka_rahasia:
-            print("Terlalu rendah!")
-        elif tebakan > angka_rahasia:
-            print("Terlalu tinggi!")
-
-    print("Selamat, tebakanmu benar!")
-
-tebak_angka()

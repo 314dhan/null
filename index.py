69f7009d00bb5c99a4c9e909b5bd9e91a86a3b70
dhan <ramadhan25321@gmail.com>
2023-09-29 15:47:47 +0700

    c

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
-for i in range(5):
-    for j in range(i):
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print(" ", end="")
+    for j in range(2*i+1):
         print("*", end="")
     print()
+for i in range(n-1):
+    for j in range(i+2):
+        print(" ", end="")
+    for j in range(2*(n-i-1)-1):
+        print("*", end="")
+    print()
+
+

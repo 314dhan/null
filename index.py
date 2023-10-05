bf4841bbc7d5c145c0c34db16a19a3efe1bb8d5c
dhan <ramadhan25321@gmail.com>
2023-10-05 14:50:22 +0700

    ch

--- index.py
+++ index.py
@@ -1,15 +1,5 @@
-n = 10
-for i in range(n):
-    for j in range(n-i):
-        print(" ", end="")
-    for j in range(2*i+1):
-        print("*", end="")
-    print()
-for i in range(n-1):
-    for j in range(i+2):
-        print(" ", end="")
-    for j in range(2*(n-i-1)-1):
-        print("*", end="")
-    print()
+def segitiga(n):
+    for i in range(1, n+1):
+        print(" "*(n-i) + "*"*i)
 
-
+segitiga(5)

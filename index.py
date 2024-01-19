b8050eb708089b42b555d5d856c1e354916a0228
dhan <ramadhan25321@gmail.com>
2024-01-19 16:02:36 +0700

    index

--- index.py
+++ index.py
@@ -5,4 +5,16 @@ def find_middle(arr):
     else:
         return arr[middle_index]
 
-find_middle([102030])
+def fizzbuss(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "fizzbuzz"
+    elif n % 3 == 0:
+        return "fizz"
+    elif n % 5 == 0:
+        return "buzz"
+    return n
+
+# for i in range(1, 31):
+#     print(fizzbuss(i))
+
+print(*map(lambda i: 'Fizz'*(not i%3)+'Buzz'*(not i%5) or i, range(1,31)),sep='\n')

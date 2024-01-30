3e888bbef447fb6fc6f4959aaf372f5019e06241
dhan <ramadhan25321@gmail.com>
2024-01-30 12:18:02 +0700

    c

--- index.py
+++ index.py
@@ -1,20 +1,25 @@
-def find_middle(arr):
-    middle_index = len(arr) // 2
-    if len(arr) % 2 == 0:
-        return (arr[middle_index - 1] + arr[middle_index]) / 2.0
-    else:
-        return arr[middle_index]
+import phonenumbers
+from phonenumbers import geocoder
+phonenumber1 = phonenumbers.parse("+6289501700818")
+print(geocoder.description_for_number(phonenumber1, 'en'))
 
-def fizzbuss(n):
-    if n % 3 == 0 and n % 5 == 0:
-        return "fizzbuzz"
-    elif n % 3 == 0:
-        return "fizz"
-    elif n % 5 == 0:
-        return "buzz"
-    return n
-
-# for i in range(1, 31):
-#     print(fizzbuss(i))
-
-print(*map(lambda i: 'Fizz'*(not i%3)+'Buzz'*(not i%5) or i, range(1,31)),sep='\n')
+# def find_middle(arr):
+#     middle_index = len(arr) // 2
+#     if len(arr) % 2 == 0:
+#         return (arr[middle_index - 1] + arr[middle_index]) / 2.0
+#     else:
+#         return arr[middle_index]
+#
+# def fizzbuss(n):
+#     if n % 3 == 0 and n % 5 == 0:
+#         return "fizzbuzz"
+#     elif n % 3 == 0:
+#         return "fizz"
+#     elif n % 5 == 0:
+#         return "buzz"
+#     return n
+#
+# # for i in range(1, 31):
+# #     print(fizzbuss(i))
+#
+# print(*map(lambda i: 'Fizz'*(not i%3)+'Buzz'*(not i%5) or i, range(1,31)),sep='\n')

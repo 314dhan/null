f5e2e9098c0b0163e1208b10f6fefb1fd326bb07
dhan <ramadhan25321@gmail.com>
2024-02-17 19:31:33 +0700

    binary search

---
+++ bianarySearch.py
@@ -0,0 +1,17 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+x = 10
+result = binary_search(arr, x)
+print(result)

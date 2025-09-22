2132109a45e4a3bb1d261d50263127b4b02e2490
dhan <ramadhan25321@gmail.com>
2025-09-22 15:01:18 +0700

    update

--- bot.py
+++ bot.py
@@ -12,15 +12,14 @@
 # # Panggil fungsi untuk menjalankan proses
 # type_text_and_press_enter()
 
-import os , random
+import os
 
 for i in range(400):
-    d = str(i) + 'days ago'
-    rand = random.randrange(1, 12)
+    d = str(i) + ' days ago'
     with open('test.txt','a') as file:
         file.write(d+'\n')
     os.system('git add test.txt')
-    os.system('git commit --date=" 2025-'+str(rand)+'-'+d+'" -m 1')
+    os.system(f'git commit --date="{d}" -m "Commit from {d}"')
 os.system('git push -u origin main')
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600"
